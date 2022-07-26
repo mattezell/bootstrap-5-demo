b071469e0aa5761d99f152810ed5ed340d3572ef
Matt Ezell <ezell.matt@gmail.com>
2022-07-26 06:10:43 -0500

    AppComponent - initial Bootstrap Modal setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Modal } from 'bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  ngAfterViewInit(): void {
+    throw new Error('Method not implemented.');
+  }
   title = 'bootstrap-5-demo';
 }
